d87f8a7c1abb996659b92e02cd5b99fe32779672
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-14 16:54:27 -0400

    chore: change mark as read type request from post to patch

--- src/modules/notifications/infra/http/routes/notifications.routes.ts
+++ src/modules/notifications/infra/http/routes/notifications.routes.ts
@@ -7,7 +7,7 @@ const notificationsRouter = Router();
 notificationsRouter.get('/', NotificationsController.index);
 notificationsRouter.get('/:id', NotificationsController.show);
 notificationsRouter.post('/', NotificationsController.create);
-notificationsRouter.post('/:id', NotificationsController.update);
+notificationsRouter.patch('/:id', NotificationsController.update);
 notificationsRouter.delete('/:id', NotificationsController.delete);
 
 export default notificationsRouter;
