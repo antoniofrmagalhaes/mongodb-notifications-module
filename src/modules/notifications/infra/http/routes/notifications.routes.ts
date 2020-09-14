82571a76ebb221cc4e74593be29c5e32893cda0a
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-14 16:27:32 -0400

    feat: notifications controller methods set

--- src/modules/notifications/infra/http/routes/notifications.routes.ts
+++ src/modules/notifications/infra/http/routes/notifications.routes.ts
@@ -1,5 +1,13 @@
 import { Router } from 'express';
 
+import NotificationsController from '../controllers/NotificationsControllers';
+
 const notificationsRouter = Router();
+
+notificationsRouter.get('/', NotificationsController.index);
+notificationsRouter.get('/:id', NotificationsController.show);
+notificationsRouter.post('/', NotificationsController.create);
+notificationsRouter.post('/:id', NotificationsController.update);
+notificationsRouter.delete('/:id', NotificationsController.delete);
 
 export default notificationsRouter;
