c89576d0891c765fc7ac886b143f29827056e426
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-14 16:09:11 -0400

    feat: notifications routes set

--- src/core/routes/index.ts
+++ src/core/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import notificationsRouter from '../../modules/notifications/infra/http/routes/notifications.routes';
+
 const routes = Router();
 
 routes.get('/', (request, response) => {
   return response.json({ status: 'ok' });
 });
+
+routes.use('/notifications', notificationsRouter);
 
 export default routes;
